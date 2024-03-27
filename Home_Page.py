31fdf995a903ac900eecd5a10a5fae936206ebc6
pgarrett <pgarrett@scripps.edu>
2024-03-27 15:52:23 -0700

    Other apps

--- Home_Page.py
+++ Home_Page.py
@@ -5,7 +5,13 @@ st.title("Home Page :house_with_garden:")
 
 st.markdown("A collection of Streamlit apps that demonstrate the use of the [peptacular](https://pypi.org/project/peptacular/) library.")
 
+st.subheader("Apps:")
 st.page_link(page="pages/Amino_Acid_Mass_Calculator.py", label="Amino Acid Mass Calculator", icon="📱")
 st.page_link(page="pages/Chemical_Formula_Calculator.py", label="Chemical Formula Calculator", icon="🧪")
 st.page_link(page="pages/Element_Mass_Table.py", label="Elemental Mass Table", icon="🧮")
 st.page_link(page="pages/Modification_Mass_Table.py", label="Modification Mass Table", icon="🧰")
+
+st.subheader("Other Apps:")
+st.page_link(page="https://proteincleaver-dev.streamlit.app/", label="Protein Cleaver", icon="🔪")
+st.page_link(page="https://peptide-fragmenter-dev.streamlit.app/", label="Peptide Fragmenter", icon="💣")
+st.page_link(page="https://spectra-viewer-dev.streamlit.app/", label="Spectra Viewer", icon="👓")
